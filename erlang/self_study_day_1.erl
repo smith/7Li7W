81f5bca7bcbdd628f1b539025ce0c13691c2a1e5
Nathan L Smith <nlloyds@gmail.com>
2013-04-11 00:13:06 -0500

    add most of first day of erlang

---
+++ erlang/self_study_day_1.erl
@@ -0,0 +1,11 @@
+-module(self_study_day_1).
+-export([words_in/1]).
+-export([count_to_ten/0]).
+-export([success_or_error/1]).
+
+words_in(String) -> todo.
+
+count_to_ten() -> todo.
+
+success_or_error(success)          -> success;
+success_or_error({error, Message}) -> Message.
